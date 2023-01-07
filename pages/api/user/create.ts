305d7359ba28aeeca300e63058fe46d8968f00b3
San_ThureinOo <santhueinoo@gmail.com>
2023-01-07 19:50:57 +0630

    KP : Fixed deployment bug

--- pages/api/user/create.ts
+++ pages/api/user/create.ts
@@ -17,10 +17,10 @@ export default function handler(
 
     // Guard clause checks for first and last name,
     // and returns early if they are not found
-    if (!body.name || !body.password) {
-        // Sends a HTTP bad request error code
-        return res.status(400).json({ data: 'First or last name not found' })
-    }
+    // if (!body.name || !body.password) {
+    //     // Sends a HTTP bad request error code
+    //     return res.status(400).json({ data: 'First or last name not found' })
+    // }
     
     try {
         excuteQuery({
